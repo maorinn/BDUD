cb9f4bda4a6cd226a5d856b65dee8bd1b0c65f81
maoring <maoring21@gmailc.com>
2021-09-28 00:27:42 +0800

    i

--- test.go
+++ test.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"os"
-)
+
 
 func main() {
-	err:=os.Rename("./download/台灣SWAG.mp4.mp4","/root/googleDrive/台灣SWAG.mp4.mp4")
+	err:=MoveFile("./download/台灣SWAG.mp4.mp4","/root/googleDrive/台灣SWAG.mp4.mp4")
 	if err != nil {
 		panic(err)
 	}
